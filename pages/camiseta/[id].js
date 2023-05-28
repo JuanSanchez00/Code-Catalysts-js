f02c76b56fa19146f884a46506bd7f3c497262ec
Genaro08 <genaro.08@hotmail.es>
2023-05-28 03:18:03 -0300

    arreglo ver camisetas

--- pages/camiseta/[id].js
+++ pages/camiseta/[id].js
@@ -1,34 +1,35 @@
-import {useRouter} from 'next/router';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import NavBar from '../../components/navbar';
 import PedidoCamiseta from '../../components/pedidoCamiseta';
 import { getCamisetaPorID } from "../../data/api";
 
-
-let id;
-
 export default function Index(props) {
-    id = useRouter().query.id;
-    let descripcion = props.camiseta[0].descripcion; 
+  const router = useRouter();
+  const { id } = router.query;
+  let descripcion = props.camiseta[0].descripcion; 
 
-    return (
-        <div>
-            <Head>
-                <title>{descripcion}</title>
-                <link rel="icon" href="https://i.ibb.co/7WBsHrf/Logo.png" />
-            </Head>
-            <NavBar/>
-            <PedidoCamiseta camiseta={props.camiseta[0]}/>
-        </div>
-    );
-  }
+  return (
+    <div>
+      <Head>
+        <title>{descripcion}</title>
+        <link rel="icon" href="https://i.ibb.co/7WBsHrf/Logo.png" />
+      </Head>
+      <NavBar/>
+      <PedidoCamiseta camiseta={props.camiseta[0]}/>
+    </div>
+  );
+}
 
-  export async function getServerSideProps() {
-    let camiseta;
-    camiseta = await getCamisetaPorID(id);
-    return {
-      props: {
-        camiseta,
-      },
-    };
-  }
+export async function getServerSideProps(context) {
+  const { id } = context.query;
+
+  let camiseta;
+  camiseta = await getCamisetaPorID(id);
+  
+  return {
+    props: {
+      camiseta,
+    },
+  };
+}
